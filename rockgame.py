604ee8b4b79e467c30f563763316ac0084ec59d9
tomtomcoder155 <45235721+tomtomcoder155@users.noreply.github.com>
2018-12-18 12:58:35 -0700

    Update rockgame.py

--- rockgame.py
+++ rockgame.py
@@ -1,15 +1,15 @@
 import random
 y = random.randint(1,3)
 x = ['rock', 'paper', 'scissors']
-print(f'Play rock, paper, scissors!')
+print('Play rock, paper, scissors!')
 player = input('Enter your wepon here: ').lower()
+if player == x:
+    print('Its a tie!')
 if y == 1:
     print(f'Computer plays {x[0]}')
 elif y == 2:
     print(f'Computer plays {x[1]}')
 elif y == 3:
     print(f'computer plays {x[2]}')
-if player == x:
-    print('Its a tie!')
 while player == x:
     continue
